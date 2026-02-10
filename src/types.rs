ea4607c793c3e8a18258a6111de08b616cd2e803
Vicente <vicentefelipechile@gmail.com>
2026-02-10 17:58:27 -0300

    Release 1.11.1

--- src/types.rs
+++ src/types.rs
@@ -37,7 +37,7 @@ pub struct SessionState {
 }
 
 /// Application version constant
-pub const APP_VERSION: &str = "1.11.0";
+pub const APP_VERSION: &str = "1.11.1";
 
 /// Application name constant
 pub const APP_NAME: &str = "YT ShortMaker";
